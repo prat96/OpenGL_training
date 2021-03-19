ff3c2a6ec4d1b068dabd9798b7433f268abd32bb
prat96 <pratheekb96@gmail.com>
2021-03-19 21:54:59 +0100

    Add LoadShader()

--- shader.cpp
+++ shader.cpp
@@ -1,1 +1,37 @@
 #include "shader.h"
+#include <fstream>
+#include <iostream>
+
+shader::shader(const std::string& filename)
+{
+
+}
+
+shader::~shader()
+{
+
+}
+
+std::string shader::LoadShader(const std::string& filename)
+{
+	std::ifstream file;
+	file.open((filename).c_str());
+
+    std::string output;
+    std::string line;
+
+    if (file.is_open())
+    {
+        while (file.good())
+        {
+            getline(file, line);
+            output.append(line + "\n");
+        }
+    }
+    else
+    {
+        std::cerr << "Unable to load shader: " << filename << std::endl;
+    }
+
+    return output;
+}
--- shader.h
+++ shader.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <string>
+#include <GL/glew.h>
+
 class shader
 {
+public:
+	shader(const std::string& filename);
+	void Bind();
+	virtual ~shader();
+private:
+	static const unsigned int NUM_SHADERS = 2;
+	GLuint m_program;
+	GLuint m_shaders[NUM_SHADERS];
+	 
+	std::string LoadShader(const std::string& filename);
+protected:
 };
 
