3e0b6467296bae9d99b825a1830179b820361d1a
prat96 <pratheekb96@gmail.com>
2021-03-21 14:51:40 +0100

    Add Vertex class

--- Mesh.h
+++ Mesh.h
@@ -1,4 +1,19 @@
 #pragma once
+
+#include <glm/glm.hpp>
+
+class Vertex 
+{
+public:
+	Vertex(const glm::vec3& pos)
+	{
+		this->pos = pos;
+	}
+protected:
+private:
+	glm::vec3 pos;
+};
+
 class Mesh
 {
 public:
